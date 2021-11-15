8fc4f8a3c011cd9deb2b6fb792389398f9ccdc94
Alessandro-Costa <alessandro.costa.ing@gmail.com>
2021-11-15 17:45:54 +0100

    quasi quasi

--- PrimeNumber/python-docker/curl.sh
+++ PrimeNumber/python-docker/curl.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
-while getopts "n:s" opt; do
-  case $opt in
-    n) 
-		echo "-n was triggere, Parameter: $OPTARG"   value=$OPTARG;;
-  esac
-done
-
-exec curl -XPOST "http://localhost:9001/2015-03-31/functions/function/invocations" -d '{$value}'
-
+curl -XPOST http://localhost:9001/2015-03-31/functions/function/invocations -d '{"operation":"pi","value":100}'
--- PrimeNumber/python-docker/docker.sh
+++ PrimeNumber/python-docker/docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker build -t cpu_intensive:1.0 .
-docker run -p 9001:8080 -v ~/.aws/:/root/.aws/ cpu_intensive:1.0
+#docker build -t cpu_intensive:1.0 .
+gnome-terminal -- docker run -p 9001:8080 -v ~/.aws/:/root/.aws/ cpu_intensive:1.0
+gnome-terminal -- docker stats
+
 
