09780af5105a3bdec06752ef54066637534c9f12
Alessandro-Costa <alessandro.costa.ing@gmail.com>
2021-11-22 14:22:31 +0100

    final version

--- B3/docker.sh
+++ B3/docker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 OUTPUT=$(pwd)
 echo "${OUTPUT}"
-cd ${OUTPUT}/PrimeNumber/Client/
+sudo apt-get install update
+sudo apt-get install docker
+sudo apt-get install gnome-terminal
+cd ${OUTPUT}/Project/Client/
 docker build -t client:1.0 .
 gnome-terminal -- docker run -it client:1.0 /bin/bash
-cd ${OUTPUT}/PrimeNumber/lambda-image
+cd ${OUTPUT}/Project/lambda-image
 docker build -t edge:1.0 .
-gnome-terminal -- docker run -p 9001:8080 -v ~/.aws/:/root/.aws/ edge:1.0
+gnome-terminal -- docker run  --cpus="1.5" -p 9001:8080 -v ~/.aws/:/root/.aws/ edge:1.0
 gnome-terminal -- docker run -p 9002:8080 -v ~/.aws/:/root/.aws/ edge:1.0
 gnome-terminal -- docker stats
 
